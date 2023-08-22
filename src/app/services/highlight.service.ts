8f80749cc170ccc95e89f3f49bcdc07a497088b7
Cramzzzberry <janroebantuan@gmail.com>
2023-08-22 16:07:22 +0800

    removed copy to clipboard first

--- src/app/services/highlight.service.ts
+++ src/app/services/highlight.service.ts
@@ -3,10 +3,9 @@ import { Injectable, Inject } from '@angular/core';
 import { PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
-// import 'clipboard';
 import 'prismjs';
 import 'prismjs/plugins/toolbar/prism-toolbar';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+// import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-jsx.min';
